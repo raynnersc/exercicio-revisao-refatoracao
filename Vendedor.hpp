8e1b33583568c85497623c31e637aa4d2f9196ee
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:15:02 -0300

    Update Vendedor.hpp

--- Vendedor.hpp
+++ Vendedor.hpp
@@ -1,1 +1,18 @@
+#ifndef VENDEDOR_H
+#define VENDEDOR_H
 
+#include <string>
+#include "Empregado.hpp"
+
+class Vendedor : public Empregado
+{
+  public:
+    std::string get_nome();
+    void set_nome(std::string _nome);	  
+    double quotaTotalAnual();
+    void print(double horasTrabalhadas) override;
+	private:
+    std::string nome;
+};
+
+#endif
