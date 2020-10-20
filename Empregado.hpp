5ad48c0b72653ffc4fb54cdd9cb72b34f5d9594a
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:12:54 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -5,11 +5,16 @@
 #include <string>
 
 class Empregado {
-	
   public:
+    double pagamentoMes(double horasTrabalhadas);
+    double get_salarioHora();
+    double get_quotaMensalVendas();
+    void set_salarioHora(double _salarioHora);
+    void set_quotaMensalVendas(double _quotaMensalVendas);
+    virtual void print(double horasTrabalhadas);
+  private:
     double salarioHora;  
     double quotaMensalVendas;
-    double pagamentoMes(double horasTrabalhadas);	
 };
 
 #endif
