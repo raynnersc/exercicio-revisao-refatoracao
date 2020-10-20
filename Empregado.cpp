2e3573705afc3d26e06bdb80dd15f19b08d7ae44
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:12:33 -0300

    Update Empregado.cpp

--- Empregado.cpp
+++ Empregado.cpp
@@ -1,16 +1,39 @@
-#include <iostream>
+#include "Empregado.hpp"
 #include <string>
+#include <iostream>
 
 #define HorasNormais 8
 
-double Empregado::pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > HorasNormais) {
-        double x = horasTrabalhadas - HorasNormais;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
+double Empregado::pagamentoMes(double horasTrabalhadas)
+{
+  double t = horasTrabalhadas; 
+	//Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+  if (horasTrabalhadas > HorasNormais) 
+  {
+    double x = horasTrabalhadas - HorasNormais;
+    t += x / 2;
+  }
+	return t * salarioHora;
+}
+
+double Empregado::get_salarioHora()
+{
+  return salarioHora;
+}
+
+double Empregado::get_quotaMensalVendas()
+{
+  return quotaMensalVendas;
+}
+
+void Empregado::set_salarioHora(double _salarioHora)
+{
+  salarioHora = _salarioHora;
+}
+
+void Empregado::set_quotaMensalVendas(double _quotaMensalVendas)
+{
+  quotaMensalVendas = _quotaMensalVendas;
+}
+
+void Empregado::print(double horasTrabalhadas) {}
