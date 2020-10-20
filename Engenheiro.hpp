3742ca926113b5a13539d56488325353765b4441
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:14:08 -0300

    Update Engenheiro.hpp

--- Engenheiro.hpp
+++ Engenheiro.hpp
@@ -1,1 +1,20 @@
+#ifndef ENGENHEIRO_H
+#define ENGENHEIRO_H
 
+#include <string>
+#include "Empregado.hpp"
+
+class Engenheiro : public Empregado
+{
+  public:
+    int get_projetos();
+    void set_projetos(int _projetos);
+    std::string get_nome();
+    void set_nome(std::string _nome);
+    void print(double horasTrabalhadas) override;
+  private:
+    std::string nome;  
+	  int projetos;
+};
+
+#endif
