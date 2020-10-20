ff25a12c0a3685c1b03765e99f3152ae95dc2fff
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:13:22 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,30 @@
 #include <string>
-#include "Empregado.hpp"
+#include "Engenheiro.hpp"
 
-class Engenheiro : public Empregado {
+int Engenheiro::get_projetos()
+{
+  return projetos;
+}
 
-  public:
-    std::string nome;  
-	int projetos;
-	
-};
+void Engenheiro::set_projetos(int _projetos)
+{
+  projetos = _projetos;
+}
 
+std::string Engenheiro::get_nome()
+{
+  return nome;
+}
+
+void Engenheiro::set_nome(std::string _nome)
+{
+  nome = _nome;
+}
+
+void Engenheiro::print(double horasTrabalhadas)
+{
+  std::cout << "Nome: " << get_nome() << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;
+  std::cout << "Projetos: " << get_projetos() << std::endl;
+  std::cout << std::endl;
+}
