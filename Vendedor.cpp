b497acdb48de37222cb7adb397056fe9b5a53faa
raynnersc <69306514+raynnersc@users.noreply.github.com>
2020-10-20 02:14:38 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,27 @@
 #include <string>
-#include "Empregado.hpp"
+#include "Vendedor.hpp"
 
-class Vendedor : public Empregado {
+#define meses 12
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
-};
+std::string Vendedor::get_nome()
+{
+  return nome;
+}
 
+void Vendedor::set_nome(std::string _nome)
+{
+  nome = _nome;
+}
+
+double Vendedor::quotaTotalAnual()
+{
+  return get_quotaMensalVendas() * meses;
+}
+
+void Vendedor::print(double horasTrabalhadas)
+{
+  std::cout << "Nome: " << get_nome() << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;  
+  std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+  std::cout << std::endl;
+}
